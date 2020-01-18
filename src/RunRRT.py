fda8e7afefbd51cf4a604db56455a5d7e051ae25
Kyle <ksalted88@yahoo.com>
2020-01-18 10:58:10 -0800

    Changed the map walls to be more fun.

--- src/RunRRT.py
+++ src/RunRRT.py
@@ -4,10 +4,10 @@ from RRTMap import Node
 from RRTAlgorithim import RRT
 
 
-mapDisplay = MapDisplay([[Node(100,100),Node(100,200)],[Node(0,200),Node(100,200)],[Node(300,300),Node(100,100)],[Node(300,300),Node(450,300)]])
+mapDisplay = MapDisplay([[Node(100,100),Node(100,200)],[Node(20,200),Node(100,200)],[Node(300,300),Node(100,100)],[Node(300,300),Node(300,400)],[Node(300,400),Node(200,400)]])
 mapDisplay.drawMap()
-node1 = Node(50, 50)
-node2 = Node(250, 450)
+node1 = Node(50, 150)
+node2 = Node(125, 175)
 rrt = RRT(node1,node2,mapDisplay)
 while not rrt.explore():
     rrt.mapDisplay.drawGraph(rrt.Tree, rrt.end_node)
